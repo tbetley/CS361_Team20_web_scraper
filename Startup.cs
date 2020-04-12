cf9ca6a045ef3ac7edf35b6efc404fd3f5b612ad
Tyler Betley <tyler.betley@gmail.com>
2020-04-12 11:28:01 -0500

    Add project scaffolding

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace web_scraper
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This methodgets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
